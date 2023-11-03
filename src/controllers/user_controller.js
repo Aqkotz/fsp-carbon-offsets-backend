e76598d3b705e16bd7069d2d24ce60eeac1aaa56
Andy Kotz <andy.lyme@mac.com>
2023-11-03 16:11:12 -0400

    added auth routes

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-plusplus */
+import jwt from 'jwt-simple';
+import User from '../models/user_model';
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.json(users);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const userId = req.user._id;
+  try {
+    const user = await User.findById(userId);
+    return res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const signin = (user) => {
+  // Here we log the user in and return a token
+  return tokenForUser(user);
+};
+
+export async function createUser({ email, password, username }) {
+  // See if a user with the given email exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    // If a user with email does exist, return an error
+    throw new Error('Email is in use');
+  }
+
+  const user = new User();
+
+  user.email = email;
+  user.username = username;
+  user.password = password;
+
+  try {
+    const newUser = await user.save();
+    const token = tokenForUser(newUser);
+    console.log('savedUser', newUser);
+    return ({ newUser, token });
+  } catch (error) {
+    throw new Error(`create user error: ${error}`);
+  }
+}
+
+// encodes a new token for a user object
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.AUTH_SECRET);
+}
---
+++ src/models/user_model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new Schema({
+  username: String,
+  password: { type: String },
+  email: { type: String, unique: true, lowercase: true },
+  role: { type: String, default: 'user' },
+}, {
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+});
+
+UserSchema.pre('save', async function beforeyYourModelSave(next) {
+  // this is a reference to our model
+  // the function runs in some other context so DO NOT bind it
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  try {
+    // salt, hash, then set password to hash
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// function to compare a password and return a boolean
+UserSchema.methods.comparePassword = async function comparePassword(candidatePassword) {
+  console.log('comparePassword', candidatePassword, this.password);
+  const comparison = await bcrypt.compare(candidatePassword, this.password);
+  return comparison;
+};
+
+const UserModel = mongoose.model('User', UserSchema);
+
+export default UserModel;

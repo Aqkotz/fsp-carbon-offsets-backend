b3414b5059e4f0e378b47f34673e1acb3f5d604b
Andy Kotz <andy.lyme@mac.com>
2024-01-19 10:56:28 -0500

    fixed delete

--- src/controllers/user_goal_controller.js
+++ src/controllers/user_goal_controller.js
@@ -42,7 +42,7 @@ export const setGoal = async (req, res) => {
 
 export const deleteGoal = async (req, res) => {
   try {
-    const { id } = req.body;
+    const { id } = req.params;
     console.log(id);
     const user = await User.findById(req.user._id).populate('goals');
     const goal = await UserGoal.findById(id);

7a164b1fe5ca50d97400330cdc0d2a3211e727ec
Andy Kotz <andy.lyme@mac.com>
2024-01-21 15:14:48 -0500

    df

--- src/controllers/user_goal_controller.js
+++ src/controllers/user_goal_controller.js
@@ -34,7 +34,7 @@ export const setGoal = async (req, res) => {
     await goal.save();
     user.goals.push(goal);
     await user.save();
-    return res.json(user.populate('goals').goals);
+    return res.json(await user.populate('goals').goals);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }

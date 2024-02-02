c4758761e7cf3cce434831d71caf6d1f50b31f12
Andy Kotz <andy.lyme@mac.com>
2024-02-02 13:17:07 -0500

    changed required values

--- src/models/trip_model.js
+++ src/models/trip_model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const tripSchema = new mongoose.Schema({
   legs: { type: [String], required: true },
-  actualCarbonFootprint: { type: Number, required: true },
+  actualCarbonFootprint: { type: Number },
   modeOfTravel: { type: String, enum: ['air', 'rail', 'car'], required: true },
   potentialCarbonFootprint: {
-    air: { type: Number, required: true },
-    rail: { type: Number, required: true },
-    car: { type: Number, required: true },
+    air: { type: Number },
+    rail: { type: Number },
+    car: { type: Number },
   },
   isStale: { type: Boolean, default: true },
 });

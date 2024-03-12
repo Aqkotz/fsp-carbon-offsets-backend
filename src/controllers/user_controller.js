b58d6a8ac23976f5c364cbde02d360c26094ab1c
Andy Kotz <andy.lyme@mac.com>
2024-03-12 00:32:17 -0400

    fixed everything

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -138,6 +138,7 @@ export async function updateUserCarbonFootprint(user) {
     const startDateTimestamp = !team ? 0 : (team.startDate instanceof Date ? team.startDate.getTime() : team.startDate);
     const weekStartDateTimestamp = !team ? 0 : startDateTimestamp + (week - 1) * 7 * 24 * 60 * 60 * 1000;
     const weekStartDate = !team ? Date.now() - 7 * 24 * 60 * 60 * 1000 : new Date(weekStartDateTimestamp);
+    weekStartDate.setHours(0, 0, 0, 0);
     const lastWeekStartDate = new Date(weekStartDate);
     lastWeekStartDate.setDate(lastWeekStartDate.getDate() - 7);
 
